3f70bbf3eb2f64c3c3cca5d50cd23ffea37b554d
Poojit-Matukumalli <poojit.matukumalli@gmail.com>
2026-07-20 09:40:45 +0000

    feat(events): add log event & reduce redundant data in vire errors

--- shared/events/events.py
+++ shared/events/events.py
@@ -2,6 +2,7 @@
 All the Vire specific events.
 """
 
+from concurrent.futures import ProcessPoolExecutor
 from dataclasses import dataclass
 
 from shared.utils.types import Severity
@@ -15,3 +16,38 @@ class GCReapEvent(VireBaseEvent):
 
     write_log: bool = True
     propagate_state: bool = True
+
+@dataclass(slots=True, kw_only=True)
+class LogEvent(VireBaseEvent):
+    
+    """
+    This event is a special child instance of BaseEvent.
+    Use this to send log events to the handler.
+
+    By default job_uuid, user uuid are "SYSTEM".
+
+    By default write_log is True and propagate_state is False.
+
+    Internal log extras:
+    ---
+    exception_name, source, internal_log
+    """
+    job_uuid: str = "SYSTEM"
+    user_uuid: str = "SYSTEM"
+    event: str = "Log Event"
+    
+    source: str
+    exception_name: str
+    internal_log: str | None
+
+    write_log:bool = True
+    propagate_state: bool = False
+    
+
+    def get_log_extras(self) -> dict[str, str]:
+        log_extras = {
+            "exception_name": self.exception_name,
+            "source":self.source,
+        }
+        log_extras.update({"reason":self.internal_log} if self.internal_log else {})
+        return log_extras
